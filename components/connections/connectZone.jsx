bceca2bac244f00d78e46473da9510a81ecce0e4
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-07-21 13:46:30 +0200

    Add layout and connectZone component

---
+++ components/connections/connectZone.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Card from "../ui/Card";
+import styles from "./connectZone.module.css";
+
+function MeetupItem(props) {
+  return (
+    <li className={styles.item}>
+      <Card>
+        <div className={styles.image}>
+          <Image src={props.image} alt={props.title} />
+        </div>
+        <div className={styles.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+        </div>
+        <div className={styles.actions}>
+          <button>Show Details</button>
+        </div>
+      </Card>
+    </li>
+  );
+}
+
+export default MeetupItem;
