e3fff84773092c6e8a51210c06f4ab7ed3534c76
brt-yilmaz <117680393+brt-yilmaz@users.noreply.github.com>
2023-07-29 23:24:46 +0200

    Remove Dummy Connect Zones and getch zones from database

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,28 @@
-import ConnectZoneList from "@/components/connections/ConnectZoneList";
+import { MongoClient } from "mongodb";
 
-const DUMMY_ZONES = [
-  {
-    id: "z1",
-    title: "First Zone",
-    image:
-      "https://media.istockphoto.com/id/494226648/de/foto/uzung%C3%B6l.jpg?s=612x612&w=0&k=20&c=UwICSjpbM3wcEfUmT6YunhgtYS3eGDc1G6rM-qDcRUA=",
-    address: " uzungol 1234 tranbzon / Turkiye",
-    description: "This is a first place to connect",
-  },
-  {
-    id: "z2",
-    title: "Second Zone",
-    image:
-      "https://media.istockphoto.com/id/1211210967/de/foto/kloster-sumela-in-trabzon-t%C3%BCrkei-das-griechisch-orthodoxe-kloster-sumela-wurde-im-4.jpg?s=612x612&w=0&k=20&c=1QmE8JnhTQKn7-566xQ78rWWF4M0txTB0U-kzVKy8Ks=",
-    address: " uzungol 1234 trabzon / Turkiye",
-    description: "This is a second place to connect",
-  },
-];
+import ConnectZoneList from "@/components/connections/ConnectZoneList";
 
 function HomePage(props) {
   return <ConnectZoneList connectZones={props.zones} />;
 }
 
 export async function getStaticProps() {
-  // fetch data from Api
+  const client = await MongoClient.connect(
+    "mongodb+srv://beratyilmaz:VNoimoaNs9gHOZJY@cluster0.edkqulq.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const zonesCollection = db.collection("new-zones");
+  const zonesArray = await zonesCollection.find().toArray();
+  client.close();
+
   return {
     props: {
-      zones: DUMMY_ZONES,
+      zones: zonesArray.map((zone) => ({
+        title: zone.title,
+        image: zone.image,
+        address: zone.address,
+        id: zone._id.toString(),
+      })),
     },
     revalidate: 10, // to regenerate page to prevent using ex-data. seconds
   };
