d5b7452e8f147bdfcaa2e9df7bfb8f6683cb54ac
brt-yilmaz <117680393+brt-yilmaz@users.noreply.github.com>
2023-07-30 01:24:10 +0200

    Fetch Single Zone Data from Database

--- pages/[connectZoneId]/index.jsx
+++ pages/[connectZoneId]/index.jsx
@@ -1,36 +1,38 @@
+import { MongoClient, ObjectId } from "mongodb";
 import ConnectZoneDetails from "@/components/connections/ConnectZoneDetails";
 import { useRouter } from "next/router";
 
-function ConnectZoneId() {
+function ConnectZoneId(props) {
   const router = useRouter();
 
   return (
     <ConnectZoneDetails
-      img={
-        "https://media.istockphoto.com/id/494226648/de/foto/uzung%C3%B6l.jpg?s=612x612&w=0&k=20&c=UwICSjpbM3wcEfUmT6YunhgtYS3eGDc1G6rM-qDcRUA="
-      }
-      title={"A First Connect Zone"}
-      address={"Uzungol 1234 tranbzon / Turkiye"}
-      description={"The Connect Zone Description"}
+      img={props.connectZoneData.image}
+      title={props.connectZoneData.title}
+      address={props.connectZoneData.address}
+      description={props.connectZoneData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://beratyilmaz:VNoimoaNs9gHOZJY@cluster0.edkqulq.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const zonesCollection = db.collection("new-zones");
+  const zonesIdArray = await zonesCollection
+    .find({})
+    .project({ _id: 1 })
+    .toArray();
+  client.close();
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          connectZoneId: "m1",
-        },
+    paths: zonesIdArray.map((zone) => ({
+      params: {
+        connectZoneId: zone._id.toString(),
       },
-      {
-        params: {
-          connectZoneId: "m2",
-        },
-      },
-    ],
+    })),
   };
 }
 
@@ -38,14 +40,25 @@ export async function getStaticProps(context) {
   // fetch data for a single zone
   const zoneId = context.params.connectZoneId;
 
+  const client = await MongoClient.connect(
+    "mongodb+srv://beratyilmaz:VNoimoaNs9gHOZJY@cluster0.edkqulq.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const zonesCollection = db.collection("new-zones");
+  const selectedZone = await zonesCollection.findOne({
+    _id: new ObjectId(zoneId),
+  });
+  console.log(selectedZone);
+  client.close();
+
   return {
     props: {
-      connectZoneId: {
-        img: "https://media.istockphoto.com/id/494226648/de/foto/uzung%C3%B6l.jpg?s=612x612&w=0&k=20&c=UwICSjpbM3wcEfUmT6YunhgtYS3eGDc1G6rM-qDcRUA=",
-        id: zoneId,
-        title: "A First Connect Zone",
-        address: "Uzungol 1234 tranbzon / Turkiye",
-        description: "The Connect Zone Description",
+      connectZoneData: {
+        id: selectedZone._id.toString(),
+        title: selectedZone.title,
+        image: selectedZone.image,
+        description: selectedZone.description,
+        address: selectedZone.address,
       },
     },
   };
