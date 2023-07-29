dc7fa772a28d45104d5feee0c1894c6d26ca8a46
brt-yilmaz <117680393+brt-yilmaz@users.noreply.github.com>
2023-07-29 23:06:13 +0200

    Connect to MongoDb to send post request

--- pages/api/new-zone.js
+++ pages/api/new-zone.js
@@ -1,11 +1,11 @@
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
-  if (req.method === "Post") {
+  if (req.method === "POST") {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://beratyilmaz3102:<Izmitli41*>@nextjs-basics.qwe55xu.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://beratyilmaz:VNoimoaNs9gHOZJY@cluster0.edkqulq.mongodb.net/?retryWrites=true&w=majority"
     );
     const db = client.db();
     const zonesCollection = db.collection("new-zones");
@@ -14,3 +14,5 @@ async function handler(req, res) {
     res.status(201).json({ message: "Zone Inserted!" });
   }
 }
+
+export default handler;
--- pages/new-connectZone/index.jsx
+++ pages/new-connectZone/index.jsx
@@ -1,8 +1,21 @@
 import NewConnectZoneForm from "@/components/connections/NewConnectZoneForm";
+import { useRouter } from "next/router";
 
 function NewMeetupPage() {
-  function handleAddZone(enteredZoneDate) {
-    console.log(enteredZoneDate);
+  const router = useRouter();
+
+  async function handleAddZone(enteredZoneData) {
+    const response = await fetch("/api/new-zone", {
+      method: "POST",
+      body: JSON.stringify(enteredZoneData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    router.push("/");
   }
 
   return <NewConnectZoneForm onAddZone={handleAddZone} />;
